dacbe14100f133ac1ab1060b2f7ad1434045d543
Sylveon <sylveon@skymizer.com>
2019-01-12 03:29:19 +0800

    Execute

---
+++ src/Core/Content.h
@@ -0,0 +1,163 @@
+#pragma once
+
+#include <Third/EVMC/include/evmc/evmc.h>
+#include <Third/EVMC/include/evmc/helpers.hpp>
+
+#include <map>
+
+struct VirtualEVMCContent : evmc_context
+{
+    struct account
+    {
+        evmc_uint256be balance = {};
+        size_t code_size = 0;
+        evmc_bytes32 code_hash = {};
+        std::map<evmc_bytes32, evmc_bytes32> storage;
+    };
+
+    evmc_tx_context tx_context = {};
+    std::map<evmc_address, account> accounts;
+};
+
+class EVMCContent
+{
+    static bool account_exists(evmc_context* context, const evmc_address* address)
+    {
+        VirtualEVMCContent* host = reinterpret_cast<VirtualEVMCContent*>(context);
+        return host->accounts.find(*address) != host->accounts.end();
+    }
+
+    static evmc_bytes32 get_storage(evmc_context* context, const evmc_address* address, const evmc_bytes32* key)
+    {
+        VirtualEVMCContent* host = reinterpret_cast<VirtualEVMCContent*>(context);
+        auto it = host->accounts.find(*address);
+        if (it != host->accounts.end())
+            return it->second.storage[*key];
+        return {};
+    }
+
+    static enum evmc_storage_status set_storage(evmc_context* context, const evmc_address* address, const evmc_bytes32* key, const evmc_bytes32* value)
+    {
+        VirtualEVMCContent* host = reinterpret_cast<VirtualEVMCContent*>(context);
+        auto& account = host->accounts[*address];
+        auto prevValue = account.storage[*key];
+        account.storage[*key] = *value;
+
+        if (prevValue == *value)
+            return EVMC_STORAGE_UNCHANGED;
+        else
+            return EVMC_STORAGE_MODIFIED;
+    }
+
+    static evmc_uint256be get_balance(evmc_context* context, const evmc_address* address)
+    {
+        VirtualEVMCContent* host = reinterpret_cast<VirtualEVMCContent*>(context);
+        auto it = host->accounts.find(*address);
+        if (it != host->accounts.end())
+            return it->second.balance;
+        return {};
+    }
+
+    static size_t get_code_size(evmc_context* context, const evmc_address* address)
+    {
+        VirtualEVMCContent* host = reinterpret_cast<VirtualEVMCContent*>(context);
+        auto it = host->accounts.find(*address);
+        if (it != host->accounts.end())
+            return it->second.code_size;
+        return 0;
+    }
+
+    static evmc_bytes32 get_code_hash(evmc_context* context, const evmc_address* address)
+    {
+        VirtualEVMCContent* host = reinterpret_cast<VirtualEVMCContent*>(context);
+        auto it = host->accounts.find(*address);
+        if (it != host->accounts.end())
+            return it->second.code_hash;
+        return {};
+    }
+
+    static size_t copy_code(evmc_context* context, const evmc_address* address, size_t code_offset, uint8_t* buffer_data, size_t buffer_size)
+    {
+        (void)context;
+        (void)address;
+        (void)code_offset;
+        (void)buffer_data;
+        (void)buffer_size;
+        return 0;
+    }
+
+    static void selfdestruct(evmc_context* context,
+                            const evmc_address* address,
+                            const evmc_address* beneficiary)
+    {
+        (void)context;
+        (void)address;
+        (void)beneficiary;
+    }
+
+    static evmc_result call(evmc_context* context, const evmc_message* msg)
+    {
+        (void)context;
+        (void)msg;
+        evmc_result result{};
+        result.status_code = EVMC_FAILURE;
+        return result;
+    }
+
+    static evmc_tx_context get_tx_context(evmc_context* context)
+    {
+        (void)context;
+        evmc_tx_context result{};
+        return result;
+    }
+
+    static evmc_bytes32 get_block_hash(evmc_context* context, int64_t number)
+    {
+        VirtualEVMCContent* host = reinterpret_cast<VirtualEVMCContent*>(context);
+        int64_t current_block_number = host->tx_context.block_number;
+
+        evmc_bytes32 example_block_hash;
+        if (number < current_block_number && number >= current_block_number - 256)
+            example_block_hash = {{1, 1, 1, 1}};
+        else
+            example_block_hash = {};
+        return example_block_hash;
+    }
+
+    static void emit_log(evmc_context* context,
+                     const evmc_address* address,
+                     const uint8_t* data,
+                     size_t data_size,
+                     const evmc_bytes32 topics[],
+                     size_t topics_count)
+    {
+        (void)context;
+        (void)address;
+        (void)data;
+        (void)data_size;
+        (void)topics;
+        (void)topics_count;
+    }
+public:
+    static evmc_context* getNewContents()
+    {
+        evmc_host_interface *host = new evmc_host_interface;
+        VirtualEVMCContent *content = new VirtualEVMCContent;
+
+        host->account_exists    = EVMCContent::account_exists;
+        host->get_storage       = EVMCContent::get_storage;
+        host->set_storage       = EVMCContent::set_storage;
+        host->get_balance       = EVMCContent::get_balance;
+        host->get_code_size     = EVMCContent::get_code_size;
+        host->get_code_hash     = EVMCContent::get_code_hash;
+        host->copy_code         = EVMCContent::copy_code;
+        host->selfdestruct      = EVMCContent::selfdestruct;
+        host->call              = EVMCContent::call;
+        host->get_tx_context    = EVMCContent::get_tx_context;
+        host->get_block_hash    = EVMCContent::get_block_hash;
+        host->emit_log          = EVMCContent::emit_log;
+
+        content->host = host;
+        return (evmc_context*)content;
+    }
+};
--- src/Core/Warper.h
+++ src/Core/Warper.h
@@ -1,3 +1,4 @@
+#include <Core/Content.h>
 #include <Third/EVMC/include/evmc/helpers.h>
 #include <Third/EVMC/include/evmc/loader.h>
 #include <Third/EVMC/include/evmc/evmc.h>
@@ -8,15 +9,27 @@
 #include <iostream>
 #include <vector>
 
+using std::cout;
+using std::endl;
+
 class VMInterface
 {
     std::string m_vmpath;
-    evmc_instance* m_instance;
+    evmc_create_fn m_vmcreater;
     evmc_loader_error_code m_error;
+
+    evmc_instance* getNewEVMCInstance()
+    {
+        if( !m_vmcreater )
+            return nullptr;
+        return m_vmcreater();
+    }
+
 public:
+
     VMInterface(std::string _vmpath):m_vmpath(_vmpath)
     {
-        m_instance = evmc_load_and_create(m_vmpath.c_str(), &m_error);
+        m_vmcreater = evmc_load(m_vmpath.c_str(), &m_error);
     }
 
     evmc_loader_error_code getLoadStatus()
@@ -31,16 +44,18 @@ public:
 
     void showVMInformation(std::ostream &out)
     {
+        evmc_instance* instance = getNewEVMCInstance();
+
         out << "VM [" << m_vmpath << "]" << std::endl;
-        if( !isOpen() )
+        if( !isOpen() || !instance )
         {
             out << "Load Failed! (" << m_error << ")" << std::endl;
             return ;
         }
 
-        out << "ABI    : " << m_instance->abi_version << std::endl;
-        out << "Name   : " << m_instance->name << std::endl;
-        out << "Version: " << m_instance->version << std::endl;
+        out << "ABI    : " << instance->abi_version << std::endl;
+        out << "Name   : " << instance->name << std::endl;
+        out << "Version: " << instance->version << std::endl;
     }
 
     static void free_result_output_data(const struct evmc_result* result)
@@ -50,105 +65,33 @@ public:
 
     evmc_result execute(const std::vector<uint8_t> &opcode, const std::vector<uint8_t> &input)
     {
-        evmc_host_interface ehost;
-        evmc_context econtext{&ehost}, *context = &econtext;
-        evmc_message emsg, *msg = &emsg;
-        enum evmc_revision rev = EVMC_LATEST_REVISION;
+        evmc_instance* instance =  getNewEVMCInstance();
+        evmc_context *context = EVMCContent::getNewContents();
 
-        msg->kind = EVMC_CALL;
-        msg->flags = 0;
-        msg->depth = 20;
-        msg->gas   = 71222; 
-        msg->input_data = input.data();
-        msg->input_size = input.size();
+        //TODO: move evmc_message instead of input
+        evmc_message msg;
+        const evmc_address addr = {{0, 1, 2}};
+        const evmc_uint256be value = {{1, 0}};
 
-        struct evmc_result ret = {.status_code = EVMC_INTERNAL_ERROR};
-        if (opcode.empty())
+        msg.sender = addr;
+        msg.destination = addr;
+        msg.value = value;
+        msg.input_data = input.data();
+        msg.input_size = input.size();
+        msg.gas = 7120000;
+        msg.depth = 20;
+
+        evmc_execute_fn exec = instance->execute;
+        struct evmc_result result = exec(instance, context, EVMC_HOMESTEAD, &msg, opcode.data(), opcode.size());
+
+        cout<<"S="<<result.status_code<<endl;
+        cout<<"G="<<result.gas_left<<endl;
+        cout<<"O="<<result.output_size<<endl;
+        for(int i=0;i<result.output_size;++i)
         {
-            // In case of empty code return a fancy error message.
-            const char* error = rev == EVMC_BYZANTIUM ? "Welcome to Byzantium!" : "Hello Ethereum!";
-            ret.output_data = (const uint8_t*)error;
-            ret.output_size = strlen(error);
-            ret.status_code = EVMC_FAILURE;
-            ret.release = NULL;  // We don't need to release the constant messages.
-            return ret;
-        }
-        
-        // Simulate executing by checking for some code patterns.
-        // Solidity inline assembly is used in the examples instead of EVM bytecode.
-
-        // Assembly: `{ mstore(0, address()) return(0, msize()) }`.
-        const char return_address[] = "\x30\x60\x00\x52\x59\x60\x00\xf3";
-
-        // Assembly: `{ sstore(0, add(sload(0), 1)) }`
-        const char counter[] = "\x60\x01\x60\x00\x54\x01\x60\x00\x55";
-
-        if (opcode.size() == strlen(return_address) &&
-            strncmp((const char*)opcode.data(), return_address, strlen(return_address)) == 0)
-        {
-            static const size_t address_size = sizeof(msg->destination);
-            uint8_t* output_data = (uint8_t*)malloc(address_size);
-            if (!output_data)
-            {
-                // malloc failed, report internal error.
-                ret.status_code = EVMC_INTERNAL_ERROR;
-                return ret;
-            }
-            memcpy(output_data, &msg->destination, address_size);
-            ret.status_code = EVMC_SUCCESS;
-            ret.output_data = output_data;
-            ret.output_size = address_size;
-            ret.release = &free_result_output_data;
-            return ret;
-        }
-        else if (opcode.size() == strlen(counter) && strncmp((const char*)opcode.data(), counter, strlen(counter)) == 0)
-        {
-            const evmc_bytes32 key = {{0}};
-            evmc_bytes32 value = context->host->get_storage(context, &msg->destination, &key);
-            value.bytes[31]++;
-            context->host->set_storage(context, &msg->destination, &key, &value);
-            ret.status_code = EVMC_SUCCESS;
-            return ret;
+            cout<< std::hex << (int) result.output_data[i] << ' '<<endl; 
         }
 
-        ret.status_code = EVMC_FAILURE;
-        ret.gas_left = 0;
-
-        return ret;
-    }
-
-    void test()
-    {
-        evmc_execute_fn exec = m_instance->execute;
-
-        evmc_host_interface ehost;
-        evmc_context econt {&ehost};
-        evmc_message emsg;
-
-        evmc_get_balance_fn ebn { [](struct evmc_context* context,  const evmc_address* address)->evmc_uint256be{
-            std::cout<<"test ebn!"<<std::endl;
-            evmc_bytes32 test;
-            for( int i=0 ; i<32 ; ++i )
-            {
-                test.bytes[i] = i;
-            }
-            return test;
-        } };
-
-        memset(&ehost, 0, sizeof(ehost));
-        ehost.get_balance = ebn;
-
-        emsg.kind = EVMC_CALL;
-        emsg.flags = 0;
-        emsg.depth = 20;
-        emsg.gas   = 71222; 
-        emsg.input_data = NULL;
-        emsg.input_size = 0;
-
-        uint8_t opcode[] = {0x42, 0x31};
-        evmc_result res = exec(m_instance, &econt, EVMC_LATEST_REVISION, &emsg, opcode, sizeof(opcode));
-
-        std::cout<<"status:"<<res.status_code<<std::endl;
-        std::cout<<"gas   :"<<res.gas_left   <<std::endl;
+        return result;
     }
 };
