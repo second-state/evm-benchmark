01daeaefd8999b1f17cf0694eccc454f40dd79af
Sylveon <sylveon@skymizer.com>
2019-01-20 19:37:14 +0800

    add SoldityBuilder example

---
+++ testcase/example_solidity/simple_contract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract HelloWorld {
+  function helloWorld() external pure returns (uint a) {
+	a = 7122;
+  }
+  function aa(uint c) external pure returns (uint b) {
+	b = c*2;
+  }
+}
+
+//aa 7323febe
+//HelloWorld c605f76c
